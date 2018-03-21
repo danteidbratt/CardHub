add7d0b1ba4146ddc1cd92372ebaecc1d3edd932
danteidbratt <32329674+danteidbratt@users.noreply.github.com>
2018-03-21 13:47:33 +0100

    HomeComponent removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DealerComponent } from './dealer/dealer.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    component: DealerComponent
   },
   {
     path: "about",
     component: AboutComponent
   },
-  {
-    path: "dealer",
-    component: DealerComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DealerComponent } from './dealer/dealer.component';
 import { CardService } from './card.service';
@@ -14,7 +13,6 @@ import { CardService } from './card.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AboutComponent,
     DealerComponent
   ],
